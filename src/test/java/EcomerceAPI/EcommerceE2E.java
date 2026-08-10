c61ba7f2342f34f5fed339c0e84ac24c950dbd63
gaurav <gauravkad139@gmail.com>
2026-08-10 09:23:42 +0530

    Add product addition functionality to EcommerceE2E

--- src/test/java/EcomerceAPI/EcommerceE2E.java
+++ src/test/java/EcomerceAPI/EcommerceE2E.java
@@ -4,7 +4,10 @@ import POJO.LogInPayload;
 import POJO.LogInRespPayload;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.specification.RequestSpecification;
+
+import java.io.File;
 
 import static io.restassured.RestAssured.given;
 
@@ -25,5 +28,23 @@ public class EcommerceE2E {
         System.out.println(resp.getToken());
         System.out.println(resp.getUserId());
         System.out.println(resp.getMessage());
+
+        RequestSpecification addProductSpecs = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com/")
+                .addHeader("Authorization", resp.getToken())
+                .build();
+
+        RequestSpecification reqAddProd = given().spec(addProductSpecs).param("productName", "Laptop")
+                .param("productAddedBy", resp.getUserId())
+                .param("productCategory", "Electronics")
+                .param("productSubCategory", "Laptop")
+                .param("productPrice", "1000")
+                .param("productDescription", "A high-end laptop")
+                .param("productFor", "women").multiPart("productImage", new File("C:\\Users\\gaura\\Desktop\\img.jpeg"));
+
+        String addProductResponse = reqAddProd.when().post("api/ecom/product/add-product").then().extract().response().asString();
+
+        JsonPath addProdJson = new JsonPath(addProductResponse);
+        String productId = addProdJson.getString("productId");
+        System.out.println(productId);
     }
 }
