977def86a4ff9b7626864815a603c20f039478af
Gaurav Kad <102280630+KadGaurav@users.noreply.github.com>
2026-08-10 08:46:47 +0530

    LogInCall (#1)

--- src/test/java/EcomerceAPI/EcommerceE2E.java
+++ src/test/java/EcomerceAPI/EcommerceE2E.java
@@ -1,4 +1,22 @@
 package EcomerceAPI;
 
+import POJO.LogInPayload;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+import static io.restassured.RestAssured.given;
+
 public class EcommerceE2E {
+
+    static void main(String[] args) {
+        RequestSpecification requests = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com/")
+                .setContentType(ContentType.JSON).build();
+
+        LogInPayload logInReq =  new LogInPayload();
+        logInReq.setUserEmail("Practice1234@gmail.com");
+        logInReq.setUserPassword("Practice1234@gmail.com");
+
+        RequestSpecification logInCall = given().spec(requests).body(logInReq);
+    }
 }
---
+++ src/test/java/POJO/LogInPayload.java
@@ -0,0 +1,23 @@
+package POJO;
+
+public class LogInPayload {
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    String userEmail;
+    String userPassword;
+
+}
